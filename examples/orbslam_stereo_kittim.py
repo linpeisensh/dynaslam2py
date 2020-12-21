13109a7e7db4592987606048a035df3005742793
Peisen <linpeisensh@gmail.com>
2020-12-21 08:53:23 +0100

    dyp

--- examples/orbslam_stereo_kittim.py
+++ examples/orbslam_stereo_kittim.py
@@ -43,19 +43,19 @@ def main(vocab_path, settings_path, sequence_path, coco_path, device):
         left_image = cv2.imread(left_filenames[idx], cv2.IMREAD_UNCHANGED)
         left_mask = get_mask(coco_demo,left_image).astype(np.uint8)
         left_mask_dil = cv2.dilate(left_mask,kernel)[:, :, None]
-        # if idx == 1:
-        #     cv2.imwrite("lm.png",left_mask*255)
-        #     cv2.imwrite("lmd.png",left_mask_dil*255)
-        # left_mask -= left_mask_dil
-        left_mask = np.ones_like(left_mask) - left_mask_dil
-        # if idx == 1:
-        #     cv2.imwrite("lma.png",left_mask*255)
-        #     break
+        if idx == 1:
+            cv2.imwrite("lm.png",left_mask*255)
+            cv2.imwrite("lmd.png",left_mask_dil*255)
+        left_mask -= left_mask_dil
+        # left_mask = np.ones_like(left_mask) - left_mask_dil
+        if idx == 1:
+            cv2.imwrite("lma.png",left_mask*255)
+            break
         right_image = cv2.imread(right_filenames[idx], cv2.IMREAD_UNCHANGED)
         right_mask = get_mask(coco_demo, right_image).astype(np.uint8)
         right_mask_dil = cv2.dilate(right_mask, kernel)[:, :, None]
-        # right_mask -= right_mask_dil
-        right_mask = np.ones_like(right_mask) - right_mask_dil
+        right_mask -= right_mask_dil
+        # right_mask = np.ones_like(right_mask) - right_mask_dil
         tframe = timestamps[idx]
         # h, w, c = left_image.shape
         # left_mask = np.ones((h,w,1)).astype(np.uint8)
