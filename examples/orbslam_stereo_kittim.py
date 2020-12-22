8f507ef2f749931b139133796502178b7672a7fd
Peisen <linpeisensh@gmail.com>
2020-12-22 17:36:49 +0100

    dyp

--- examples/orbslam_stereo_kittim.py
+++ examples/orbslam_stereo_kittim.py
@@ -41,12 +41,12 @@ def main(vocab_path, settings_path, sequence_path, coco_path, device):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(2*dilation+1,2*dilation+1))
     for idx in range(num_images):
         left_image = cv2.imread(left_filenames[idx], cv2.IMREAD_UNCHANGED)
-        left_mask = get_mask(coco_demo,left_image).astype(np.uint8)
-        left_mask_dil = cv2.dilate(left_mask,kernel)[:, :, None]
+        # left_mask = get_mask(coco_demo,left_image).astype(np.uint8)
+        # left_mask_dil = cv2.dilate(left_mask,kernel)[:, :, None]
         # if idx == 1:
         #     cv2.imwrite("lm.png",left_mask*255)
         #     cv2.imwrite("lmd.png",left_mask_dil*255)
-        left_mask = left_mask_dil
+        # left_mask = left_mask_dil
         # left_mask = left_mask_dil - left_mask
         # left_mask = np.ones_like(left_mask) - left_mask
         # left_mask = np.ones_like(left_mask) - left_mask_dil
@@ -54,16 +54,16 @@ def main(vocab_path, settings_path, sequence_path, coco_path, device):
         #     cv2.imwrite("lma.png",left_mask*255)
         #     break
         right_image = cv2.imread(right_filenames[idx], cv2.IMREAD_UNCHANGED)
-        right_mask = get_mask(coco_demo, right_image).astype(np.uint8)
-        right_mask_dil = cv2.dilate(right_mask, kernel)[:, :, None]
-        right_mask = right_mask_dil
+        # right_mask = get_mask(coco_demo, right_image).astype(np.uint8)
+        # right_mask_dil = cv2.dilate(right_mask, kernel)[:, :, None]
+        # right_mask = right_mask_dil
         # right_mask = right_mask_dil - right_mask
         # right_mask = np.ones_like(right_mask) - right_mask
         # right_mask = np.ones_like(right_mask) - right_mask_dil
         tframe = timestamps[idx]
-        # h, w, c = left_image.shape
-        # left_mask = np.ones((h,w,1)).astype(np.uint8)
-        # right_mask = np.ones((h,w,1)).astype(np.uint8)
+        h, w, c = left_image.shape
+        left_mask = np.ones((h,w,1)).astype(np.uint8)
+        right_mask = np.ones((h,w,1)).astype(np.uint8)
 
         if left_image is None:
             print("failed to load image at {0}".format(left_filenames[idx]))
