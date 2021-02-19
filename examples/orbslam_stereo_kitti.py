e04a07a16e50865df9cf0c73ef77d158e70a5b4f
Peisen <linpeisensh@gmail.com>
2021-02-19 17:45:22 +0100

    save

--- examples/orbslam_stereo_kitti.py
+++ examples/orbslam_stereo_kitti.py
@@ -83,8 +83,7 @@ def load_images(path_to_sequence):
 
 def save_trajectory(trajectory, filename):
     with open(filename, 'w') as traj_file:
-        traj_file.writelines('{time} {r00} {r01} {r02} {t0} {r10} {r11} {r12} {t1} {r20} {r21} {r22} {t2}\n'.format(
-            time=repr(stamp),
+        traj_file.writelines('{r00} {r01} {r02} {t0} {r10} {r11} {r12} {t1} {r20} {r21} {r22} {t2}\n'.format(
             r00=repr(r00),
             r01=repr(r01),
             r02=repr(r02),
@@ -98,7 +97,6 @@ def save_trajectory(trajectory, filename):
             r22=repr(r22),
             t2=repr(t2)
         ) for stamp, r00, r01, r02, t0, r10, r11, r12, t1, r20, r21, r22, t2 in trajectory)
-
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
