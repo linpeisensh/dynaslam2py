cf6133c4dd2511bec87fef5e0c7bcc88d96eb8fd
Peisen <linpeisensh@gmail.com>
2021-03-02 08:58:49 +0100

    rmse

--- examples/orbslam_stereo_kitti.py
+++ examples/orbslam_stereo_kitti.py
@@ -62,7 +62,7 @@ def main(orb_path, device, data_path, save, sequence):
         if ttrack < t:
             time.sleep(t - ttrack)
     i = 0
-    result_path = 'ro/c{}{}.txt'.format(sequence_path[-2:], i)
+    result_path = 'ro3/c{}{}.txt'.format(sequence_path[-2:], i)
     while True:
         if not os.path.exists(result_path):
             s_flag = save_trajectory(slam.get_trajectory_points(), result_path)
@@ -70,7 +70,7 @@ def main(orb_path, device, data_path, save, sequence):
                 print(result_path)
                 break
         i += 1
-        result_path = 'ro/c{}{}.txt'.format(sequence, i)
+        result_path = 'ro3/c{}{}.txt'.format(sequence, i)
 
     slam.shutdown()
 
--- examples/rmse.py
+++ examples/rmse.py
@@ -4,8 +4,9 @@ from evo.tools import log
 from evo.tools import file_interface
 import copy
 from collections import defaultdict
+import sys
 
-def get_rmse(rootdir,file,file_path):
+def get_rmse(ape_metric,file,file_path):
     traj_ref = file_interface.read_kitti_poses_file(os.path.join('poses', file[1:3] + '.txt'))
     traj_est = file_interface.read_kitti_poses_file(file_path)
     traj_est_aligned = copy.deepcopy(traj_est)
@@ -16,34 +17,49 @@ def get_rmse(rootdir,file,file_path):
     print('{} rmse: {}'.format(file, ape_stat))
     return ape_stat
 
-log.configure_logging(verbose=False, debug=False, silent=False)
-pose_relation = metrics.PoseRelation.translation_part
-ape_metric = metrics.APE(pose_relation)
-rootdir = './ro3'
-ad = defaultdict(list)
-dd = defaultdict(list)
-cd = defaultdict(list)
+def main(res_path):
+    log.configure_logging(verbose=False, debug=False, silent=False)
+    pose_relation = metrics.PoseRelation.translation_part
+    ape_metric = metrics.APE(pose_relation)
+    rootdir = res_path
+    ad = defaultdict(list)
+    dd = defaultdict(list)
+    cd = defaultdict(list)
 
-for file in sorted(os.listdir(rootdir)):
-    file_path = os.path.join(rootdir,file)
-    if os.path.isfile(file_path)  and file[-3:] == 'txt':
-        try:
-            ape_stat = get_rmse(rootdir,file,file_path)
-            if file[0] == 'a':
-                ad[file[1:3]].append(ape_stat)
-            elif file[0] == 'd':
-                dd[file[1:3]].append(ape_stat)
-            elif file[0] == 'c':
-                cd[file[1:3]].append(ape_stat)
-        except:
-            print('{} error'.format(file))
-            os.remove(file_path)
+    for file in sorted(os.listdir(rootdir)):
+        file_path = os.path.join(rootdir,file)
+        if os.path.isfile(file_path)  and file[-3:] == 'txt':
+            try:
+                ape_stat = get_rmse(ape_metric,file,file_path)
+                if file[0] == 'a':
+                    ad[file[1:3]].append(ape_stat)
+                elif file[0] == 'd':
+                    dd[file[1:3]].append(ape_stat)
+                elif file[0] == 'c':
+                    cd[file[1:3]].append(ape_stat)
+            except:
+                print('{} error'.format(file))
+                os.remove(file_path)
 
-for k, v in ad.items():
-    print('sdsr S{} mean rmse: {}'.format(k,round(sum(v)/len(v),2)))
+    for k, v in ad.items():
+        print('sdsr S{} mean rmse: {}'.format(k,round(sum(v)/len(v),2)))
 
-for k, v in dd.items():
-    print('dsr S{} mean rmse: {}'.format(k,round(sum(v)/len(v),2)))
+    total_dsr_rmse = 0
+    for k, v in dd.items():
+        rmse = round(sum(v)/len(v),2)
+        print('dsr S{} mean rmse: {}'.format(k,rmse))
+        total_dsr_rmse += rmse
+    print('total_dsr_rmse: ', total_dsr_rmse)
+    print()
 
-for k, v in cd.items():
-    print('ORB S{} mean rmse: {:.2}'.format(k,round(sum(v)/len(v),2)))
+    total_ORB_rmse = 0
+    for k, v in cd.items():
+        rmse = round(sum(v) / len(v), 2)
+        print('ORB S{} mean rmse: {:.2}'.format(k,rmse))
+        total_ORB_rmse += rmse
+    print('total_ORB_rmse: ', total_ORB_rmse)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: ./res_path ')
+    main(sys.argv[1])
