bed83923eef87cf41126d8ced609da9e048f0047
Peisen <linpeisensh@gmail.com>
2021-03-05 10:37:24 +0100

    ate,rre,rpe_fix

--- examples/rmse.py
+++ examples/rmse.py
@@ -22,7 +22,7 @@ def get_stat(ape_metric,rpe_metric, rre_metric, file,file_path):
     return ape_stat, rpe_stat, rre_stat
 
 def sort_stat(v,save_root_path,res_root_path,i,k,d):
-    if d == 'dsr' or d == 'ds':
+    if d == 'DSR' or d == 'DS':
         vres = sorted(v, key=lambda x: x[i])
     else:
         vres = sorted(v, key=lambda x: -x[i])
@@ -81,52 +81,26 @@ def main(res_root_path, save_root_path):
                 print('{} error'.format(file))
                 os.remove(file_path)
 
-    total_ds_ate = 0
-    total_ds_rpe = 0
-    total_ds_rre = 0
-    for k, v in ad.items():
-        ate = sort_stat(v, save_root_path, res_root_path, 1, k, 'ds')
-        rpe = sort_stat(v, '0', res_root_path, 2, k, 'ds')
-        rre = sort_stat(v, '0', res_root_path, 3, k, 'ds')
-        total_ds_ate += ate
-        total_ds_rpe += rpe
-        total_ds_rre += rre
+    def print_stat(xd,d):
+        total_ate = 0
+        total_rpe = 0
+        total_rre = 0
+        for k, v in xd.items():
+            rpe = sort_stat(v, '0', res_root_path, 2, k, d)
+            rre = sort_stat(v, '0', res_root_path, 3, k, d)
+            ate = sort_stat(v, save_root_path, res_root_path, 1, k, d)
+            total_ate += ate
+            total_rpe += rpe
+            total_rre += rre
+            print()
 
-    print('total_ds_rmse_ate: {}m'.format(total_ds_ate))
-    print('total_ds_mean_rpe: {}%'.format(total_ds_rpe))
-    print('total_ds_mean_rre: {}deg/100m'.format(total_ds_rre))
-    print()
-
-    total_dsr_ate = 0
-    total_dsr_rpe = 0
-    total_dsr_rre = 0
-    for k, v in dd.items():
-        ate = sort_stat(v,save_root_path,res_root_path,1,k,'dsr')
-        rpe = sort_stat(v,'0',res_root_path,2,k,'dsr')
-        rre = sort_stat(v,'0',res_root_path,3,k,'dsr')
-        total_dsr_ate += ate
-        total_dsr_rpe += rpe
-        total_dsr_rre += rre
-
-    print('total_dsr_rmse_ate: {}m'.format(total_dsr_ate))
-    print('total_dsr_mean_rpe: {}%'.format(total_dsr_rpe))
-    print('total_dsr_mean_rre: {}deg/100m'.format(total_dsr_rre))
-    print()
-
-    total_ORB_ate = 0
-    total_ORB_rpe = 0
-    total_ORB_rre = 0
-    for k, v in dd.items():
-        ate = sort_stat(v, save_root_path, res_root_path, 1, k, 'ORB')
-        rpe = sort_stat(v, '0', res_root_path, 2, k, 'ORB')
-        rre = sort_stat(v, '0', res_root_path, 3, k, 'ORB')
-        total_ORB_ate += ate
-        total_ORB_rpe += rpe
-        total_ORB_rre += rre
-
-    print('total_ORB_rmse_ate: {}m'.format(total_ORB_ate))
-    print('total_ORB_mean_rpe: {}%'.format(total_ORB_rpe))
-    print('total_ORB_mean_rre: {}deg/100m'.format(total_ORB_rre))
+        print('total_{}_mean_rpe: {}%'.format(d,total_rpe))
+        print('total_{}_mean_rre: {}deg/100m'.format(d,total_rre))
+        print('total_{}_rmse_ate: {}m'.format(d,total_ate))
+        print()
+    print_stat(ad,'DS')
+    print_stat(dd,'DSR')
+    print_stat(cd,'ORB')
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
