d3de3819b6b214ca736a4f135917a6261fb401a9
Peisen <linpeisensh@gmail.com>
2021-03-11 12:58:17 +0100

    sdsr81

--- examples/sptam/dynaseg.py
+++ examples/sptam/dynaseg.py
@@ -379,5 +379,5 @@ def norm(error, imgpts):
     if len(rm):
         ge[rma] = rm > np.percentile(rm, 93)
     if len(mm):
-        ge[mma] = mm > np.percentile(mm, 83)
+        ge[mma] = mm > np.percentile(mm, 81)
     return ge
