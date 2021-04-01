09b9db28b770e65d9a2c3de5cee1daf18f12f831
Peisen <linpeisensh@gmail.com>
2021-04-01 21:30:54 +0200

    tt

--- examples/PDSeg.py
+++ examples/PDSeg.py
@@ -86,7 +86,7 @@ class PDSeg():
                         if abs(x2 - mi) <= (x2 - x1) or abs(x1 - ma) <= (x2 - x1) or (
                                 x1 >= mi and x2 <= ma):
                             cc[mask, ...] = 255
-                    elif (x1 >= mi and x2 <= ma and self.w - x2 <= 90 and x1 <= 90):
+                    elif (x1 >= mi and x2 <= ma and self.w - x2 >= 90 and x1 >= 90):
                         cc[mask, ...] = 255
         return cc
 
